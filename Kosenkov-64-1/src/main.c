a6af660b767a41ed7668cbca2d7f0ccf9fd8efec
Aleksandr Kosenkov <Marveel@yandex.ru>
2020-05-09 21:49:38 +0300

    feat(lab1): modify done

--- Kosenkov-64-1/src/main.c
+++ Kosenkov-64-1/src/main.c
@@ -8,8 +8,9 @@
 void exit_handler();
 void switch_handler();
 
-char volatile main_loop_flag = 1;
-char volatile refresh_speed = 1;
+char main_loop_flag = 1;
+char refresh_speed = 1;
+char update_flag = 0;
 
 int main(int argc, const char **argv) {
     signal(SIGINT, SIG_IGN);
@@ -18,24 +19,42 @@ int main(int argc, const char **argv) {
     int pid = 0;
     int ppid = getpid();
 
+    int fds[2];
+    pipe(fds);
+
     if ((pid = fork()) < 0) {
         perror("fork");
         exit(-1);
     }
 
     if (pid != 0) { /* parent */
+        close(fds[1]);
         signal(SIGUSR1, exit_handler);
         signal(SIGUSR2, switch_handler);
         char output[32];
         while (main_loop_flag) {
+            if (update_flag) {
+                char read_status = read(fds[0], &refresh_speed, 1);
+                if (read_status != 1) {
+                    perror("read from pipe");
+                    main_loop_flag = 0;
+                    kill(pid, SIGKILL);
+                }
+                update_flag = 0;
+            }
+
             sprintf(output, "Alarm after %d seconds\n", refresh_speed);
             write(STDOUT_FILENO, output, strlen(output));
             sleep(refresh_speed);
         }
+        close(fds[0]);
 
     } else { /* child */
+        close(fds[0]);
         char input = 0;
         char listen_flag = 1;
+        char old_interval = refresh_speed;
+        char new_interval = 0;
         while (listen_flag) {
             char is_not_eof = 1;
             is_not_eof = read(STDIN_FILENO, &input, 1);
@@ -44,10 +63,19 @@ int main(int argc, const char **argv) {
                 kill(ppid, SIGUSR1);
                 listen_flag = 0;
             }
+            if (input >= '1' && input <= '9') {
+                /* char code to digit */
+                new_interval = input - '0';
+            } else if (input != '\n') {
+                new_interval = old_interval;
+            }
             if (input == '\n') {
+                old_interval = new_interval;
+                write(fds[1], &new_interval, 1);
                 kill(ppid, SIGUSR2);
             }
         }
+        close(fds[1]);
     }
     return 0;
 }
@@ -58,5 +86,5 @@ void exit_handler() {
 
 void switch_handler() {
     signal(SIGUSR2, switch_handler); /* reset signal */
-    refresh_speed = refresh_speed == 1 ? 3 : 1;
+    update_flag = 1;
 }
